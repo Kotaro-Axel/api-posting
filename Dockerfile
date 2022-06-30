db5ae14ac5580aa51beb7ca20f86f88b25370c5d
Kotaro-Axel <191246@ids.upchiapas.edu.mx>
2022-06-30 16:09:07 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM public.ecr.aws/ubuntu/ubuntu:20.04_stable
+RUN apt-get update
+
+RUN apt-get -y install curl
+RUN curl -sL https://deb.nodesource.com/setup_10.x  | bash -
+RUN apt-get -y install nodejs
+RUN ln -s /usr/bin/nodejs /usr/local/bin/node
+
+WORKDIR /myapp
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm","start"]
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const routes = require('./routes')
+var cors = require('cors')
+const bodyParser = require('body-parser')
+const errorhandler = require('errorhandler')
+
+let store = {
+  posts: [
+    {name: 'Testing JSON (POST)',
+    url: 'https://www.npmjs.com/',
+    text: 'Mi primera publicacion',
+    comments: [
+      {text: 'Hi from Tuxtla!'},
+      {text: 'Cheers!'},
+      {text: 'Good job'}
+    ]
+    }
+  ]
+}
+
+let app = express()
+
+app.use(cors())
+
+
+app.use(bodyParser.json())
+app.use(errorhandler())
+
+app.use((req, res, next) => {
+    req.store = store
+    next()
+})
+
+app.get('/api/posts', routes.posts.getPosts)
+app.post('/api/posts', routes.posts.addPost)
+app.put('/api/posts/:postId', routes.posts.updatePost)
+app.delete('/api/posts/:postId', routes.posts.removePost)
+
+app.get('/api/posts/:postId/comments', routes.comments.getComments)
+app.post('/api/posts/:postId/comments', routes.comments.addComment)
+app.put('/api/posts/:postId/comments/:commentId', routes.comments.updateComment)
+app.delete('/api/posts/:postId/comments/:commentId', routes.comments.removeComment)
+
+app.listen(3000)
